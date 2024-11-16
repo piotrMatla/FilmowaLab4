1e4539fb6a88bf864b43ab12d00ca1b2118cc071
piotr.matla <piotr.matla@CH-17>
2024-11-16 09:57:43 +0100

    Dodaj pliki projektów.

---
+++ FilmowaLab4/Models/Film.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmowaLab4.Models
+{
+    public class Film
+    {
+        [Key]
+        public int ID { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string Tytul { get; set; }
+        [Column(TypeName = "nvarchar(200)")]
+        public string Opis { get; set; }
+        [Column(TypeName = "int")]
+        public int Ocena { get; set; }
+    }
+}
